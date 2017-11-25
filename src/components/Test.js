81ea342610fb1c9fcdd553cca390a657de31c132
Jędrzej Ostrowski <jedrzej.ostrowski@gfactor.pl>
2017-11-25 03:07:59 +0100

    Mapa

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {Text,View} from "react-native";
+import MapView from 'react-native-maps';
 
 export default class Test extends React.Component {
   render() {
     return (
-        <View>
-                <Text> lorem ipsum</Text>
-    
+        <View style={styles.view}>
+            <MapView style={styles.map}
+                initialRegion={{
+                    latitude: 50.786131,
+                    longitude: 16.284904,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
         </View>
     );
   }
+}
+
+const styles = {
+    view: {
+        flex: 1
+    },
+    map: {
+        flex: 1
+    }
 }
    
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {Text,View,Button} from "react-native";
-import Actions from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 export default class Welcome extends React.Component {
   render() {
     return (
         <View>
-                <Text> test test</Text>
                 <Button
-                    onPress={onPressLearnMore}
-                    title="Learn More"
+                    onPress={this.onPressLearnMore}
+                    title="Przejdz do mapy"
                     color="#841584"
                     accessibilityLabel="Learn more about this purple button"
                 />
