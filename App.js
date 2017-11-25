07628e5bd04f463144fa6e5b18135f04b711e3ad
Jędrzej Ostrowski <jedrzej.ostrowski@gfactor.pl>
2017-11-25 02:38:06 +0100

    poprawki

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Router from './src/components/Router';
+
 export default class App extends React.Component {
-  render() {
-    return (
-
-
-
-    );
-  }
+    render() {
+        return (
+            <Router/>
+        );
+    }
 }
-   
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,15 @@
-
 import React from 'react';
-import { Scene,Router,Stack,ActionConst } from 'react-native-router-flux';
+import {Scene, Router, Stack, ActionConst} from 'react-native-router-flux';
 import Test from './Test';
 import Welcome from './Welcome';
+
 const RouterComponent = () => (
 
     <Router>
         <Stack>
-            <Scene  key = "welcome" component { Welcome }         />
-            <Scene  key = "test" component { Test }         />
-            </Stack>
+            <Scene key="welcome" component={Welcome}/>
+            <Scene key="test" component={Test}/>
+        </Stack>
     </Router>
 );
 export default RouterComponent;
